2ca9842c1636e2a5d6d86ae8a5ad4ee9033f0716
Coder Lakum <lakummahendra654@gmail.com>
2024-07-07 18:04:07 +0530

    fixed login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,27 +3,118 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title> Login || NighaTech Global</title>
+    <title>Login || NighaTech Global</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}">
 
-	<!-- CSS here -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="assets/css/slicknav.css">
-    <link rel="stylesheet" href="assets/css/flaticon.css">
-    <link rel="stylesheet" href="assets/css/progressbar_barfiller.css">
-    <link rel="stylesheet" href="assets/css/gijgo.css">
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-    <link rel="stylesheet" href="assets/css/animated-headline.css">
-	<link rel="stylesheet" href="assets/css/magnific-popup.css">
-	<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
-	<link rel="stylesheet" href="assets/css/themify-icons.css">
-	<link rel="stylesheet" href="assets/css/slick.css">
-	<link rel="stylesheet" href="assets/css/nice-select.css">
-	<link rel="stylesheet" href="assets/css/style.css">
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/slicknav.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/progressbar_barfiller.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/gijgo.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animated-headline.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <style>
+        .login-body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background: url('{{ asset('assets/img/login.mp4') }}') no-repeat center center fixed;
+            background-size: cover;
+        }
+
+        .login-form {
+            background: rgba(0, 0, 0, 0.7);
+            padding: 30px;
+            border-radius: 10px;
+            max-width: 400px;
+            width: 100%;
+            text-align: center;
+        }
+
+        .logo-login img {
+            width: 100px;
+            margin-bottom: 20px;
+        }
+
+        .form-input {
+            margin-bottom: 15px;
+        }
+
+        .form-input input {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+        }
+
+        .form-check {
+            text-align: left;
+        }
+
+        .form-check input {
+            margin-right: 5px;
+        }
+
+        .form-input.pt-30 input {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            background: #ff7b54;
+            color: white;
+            font-weight: bold;
+            cursor: pointer;
+        }
+
+        .forget, .registration {
+            display: block;
+            margin-top: 10px;
+            color: #ff7b54;
+            text-decoration: none;
+        }
+
+        .forget:hover, .registration:hover {
+            text-decoration: underline;
+        }
+
+        @media (max-width: 768px) {
+            .login-form {
+                padding: 20px;
+            }
+
+            .form-input input {
+                padding: 8px;
+            }
+
+            .form-input.pt-30 input {
+                padding: 8px;
+            }
+        }
+
+        @media (max-width: 480px) {
+            .logo-login img {
+                width: 80px;
+                margin-bottom: 15px;
+            }
+
+            .login-form h2 {
+                font-size: 1.5rem;
+            }
+        }
+    </style>
 </head>
 <body>
     <!-- ? Preloader Start -->
@@ -32,111 +123,98 @@
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
                 <div class="preloader-img pere-text">
-                    <img src="{{asset('assets/img/logo/loder.png')}}" alt="">
+                    <img src="{{ asset('assets/img/logo/loder.png') }}" alt="">
                 </div>
             </div>
         </div>
     </div>
-    <!-- Preloader Start-->
+    <!-- Preloader Start -->
 
+    <main class="login-body" data-vide-bg="{{ asset('assets/img/login.mp4') }}">
+        <!-- Login Admin -->
+        <form class="form-default" action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="login-form">
+                <!-- logo-login -->
+                <div class="logo-login">
+                    <a href="index.html"><img src="{{ asset('assets/img/logo/loder.png') }}" alt=""></a>
+                </div>
+                <h2>Login Here</h2>
+                <div class="form-input">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-input">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" name="password" required autocomplete="current-password" placeholder="Password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-<main class="login-body" data-vide-bg="{{ asset('assets/img/login.mp4')}}">
-    <!-- Login Admin -->
-    <form class="form-default" action="{{ route('login') }}" method="POST">
-        @csrf
-        <div class="login-form">
-            <!-- logo-login -->
-            <div class="logo-login">
-                <a href="index.html"><img src="{{ asset('assets/img/logo/loder.png')}}" alt=""></a>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="text-light" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
+                </div>
+
+                <div class="form-input pt-30">
+                    <input type="submit" name="submit" value="Login">
+                </div>
+
+                <!-- Forget Password -->
+                <a href="{{ route('password.request') }}" class="forget">Forget Password</a>
+                <!-- Forget Password -->
+                <a href="{{ route('register') }}" class="registration">Registration</a>
             </div>
-            <h2>Login Here</h2>
-            <div class="form-input">
-                <label for="name">Email</label>
-                <input  type="email" name="email" value="{{ old('email') }}"  required autocomplete="email" autofocus placeholder="Email">
+        </form>
+        <!-- /end login form -->
+    </main>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-            </div>
-            <div class="form-input">
-                <label for="name">Password</label>
-                <input id="password" type="password" name="password" required autocomplete="current-password" placeholder="password">
-                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-            </div>
-
-            <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="text-light" for="remember">
-                                        {{ __(' Remember Me') }}
-                                    </label>
-                                </div>
-
-
-            <div class="form-input pt-30">
-                <input type="submit" name="submit" value="login">
-            </div>
-
-            <!-- Forget Password -->
-            <a href="{{ route('password.request') }}" class="forget">Forget Password</a>
-            <!-- Forget Password -->
-            <a href="{{('register')}}" class="registration">Registration</a>
-        </div>
-    </form>
-    <!-- /end login form -->
-</main>
-
-
-    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="{{ asset('assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
     <!-- Jquery, Popper, Bootstrap -->
-    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
     <!-- Jquery Mobile Menu -->
-    <script src="./assets/js/jquery.slicknav.min.js"></script>
-
-    <!-- Video bg -->
-    <script src="./assets/js/jquery.vide.js"></script>
-
+    <script src="{{ asset('assets/js/jquery.slicknav.min.js') }}"></script>
     <!-- Jquery Slick , Owl-Carousel Plugins -->
-    <script src="./assets/js/owl.carousel.min.js"></script>
-    <script src="./assets/js/slick.min.js"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
     <!-- One Page, Animated-HeadLin -->
-    <script src="./assets/js/wow.min.js"></script>
-    <script src="./assets/js/animated.headline.js"></script>
-    <script src="./assets/js/jquery.magnific-popup.js"></script>
-
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/animated.headline.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.js') }}"></script>
     <!-- Date Picker -->
-    <script src="./assets/js/gijgo.min.js"></script>
+    <script src="{{ asset('assets/js/gijgo.min.js') }}"></script>
+    <!-- Video bg -->
+    <script src="{{ asset('assets/js/jquery.vide.js') }}"></script>
     <!-- Nice-select, sticky -->
-    <script src="./assets/js/jquery.nice-select.min.js"></script>
-    <script src="./assets/js/jquery.sticky.js"></script>
+    <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.sticky.js') }}"></script>
     <!-- Progress -->
-    <script src="./assets/js/jquery.barfiller.js"></script>
-
+    <script src="{{ asset('assets/js/jquery.barfiller.js') }}"></script>
     <!-- counter , waypoint,Hover Direction -->
-    <script src="./assets/js/jquery.counterup.min.js"></script>
-    <script src="./assets/js/waypoints.min.js"></script>
-    <script src="./assets/js/jquery.countdown.min.js"></script>
-    <script src="./assets/js/hover-direction-snake.min.js"></script>
-
+    <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('assets/js/hover-direction-snake.min.js') }}"></script>
     <!-- contact js -->
-    <script src="./assets/js/contact.js"></script>
-    <script src="./assets/js/jquery.form.js"></script>
-    <script src="./assets/js/jquery.validate.min.js"></script>
-    <script src="./assets/js/mail-script.js"></script>
-    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
-
+    <script src="{{ asset('assets/js/contact.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.form.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('assets/js/mail-script.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.ajaxchimp.min.js') }}"></script>
     <!-- Jquery Plugins, main Jquery -->
-    <script src="./assets/js/plugins.js"></script>
-    <script src="./assets/js/main.js"></script>
-
-    </body>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
 </html>
