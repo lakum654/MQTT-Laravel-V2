09eaad3e5b85b038f596d4cb991554963e6a8be2
pa1iot <pa1iotne@gmail.com>
2024-07-07 11:20:13 +0530

    products and service page wordpress correction removed

--- resources/views/front/pages/services.blade.php
+++ resources/views/front/pages/services.blade.php
@@ -60,47 +60,7 @@
     </section>
     <!-- Pricing Card End -->
     <!--? About-1 Area Start -->
-    <div class="about-area1 section-padding40">
-        <div class="container">
-            <div class="row align-items-center justify-content-between">
-                <div class="col-xl-5 col-lg-5 col-md-8 col-sm-10">
-                    <!-- about-img -->
-                    <div class="about-img ">
-                        <img src="{{ asset('assets/img/gallery/about1.png')}}" alt="">
-                    </div>
-                </div>
-                <div class="col-xl-7 col-lg-7 col-md-12">
-                    <div class="about-caption ">
-                        <!-- Section Tittle -->
-                        <div class="section-tittle section-tittle2 mb-30">
-                            <h2>Global server location</h2>
-                        </div>
-                        <p class="mb-40">Supercharge your WordPress hosting with detailed website analytics, marketing tools. Our experts are just part of the reason Bluehost is the ideal home for your WordPress website. We're here to help you succeed!</p>
-                        <ul>
-                            <li>
-                                 <span class="mr-3"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                    <text id="right" transform="translate(0 14)" fill="#11e276" font-size="16" font-family="FontAwesome5Free-Solid, 'Font Awesome 5 Free'"><tspan x="0" y="0"></tspan></text>
-                                  </svg></span>
-                                <p>WordPress hosting with detailed website</p>
-                            </li>
-                            <li>
-                                 <span class="mr-3"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                    <text id="right" transform="translate(0 14)" fill="#11e276" font-size="16" font-family="FontAwesome5Free-Solid, 'Font Awesome 5 Free'"><tspan x="0" y="0"></tspan></text>
-                                  </svg></span>
-                                <p>Our experts are just part of the reason</p>
-                            </li>
-                            <li>
-                                 <span class="mr-3"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                    <text id="right" transform="translate(0 14)" fill="#11e276" font-size="16" font-family="FontAwesome5Free-Solid, 'Font Awesome 5 Free'"><tspan x="0" y="0"></tspan></text>
-                                  </svg></span>
-                                <p> Detailed website analytics</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <!-- About-1 Area End -->
     <!--? About-2 Area Start -->
     <div class="about-area1 pb-bottom">
